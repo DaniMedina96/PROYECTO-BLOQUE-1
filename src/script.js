30eac7ac1221ba352c2ff20728070753ddc906d4
medina <medina@DESKTOP-28TCG6N>
2024-03-15 16:53:36 +0000

    final

--- src/script.js
+++ src/script.js
@@ -1,17 +1,3 @@
-/*function agregarAlCarrito(bicis, precio) {
-    var carrito = document.getElementById('carrito-lista');
-    var totalBicis = document.getElementById('total');
-    var total = parseInt(totalBicis.textContent)
-
-    var nuevaBici = document.createElement('p');
-    nuevaBici.textContent = bicis + ' - ' + precio + '€';
-
-    carrito.appendChild(nuevaBici);
-
-    total += precio;
-    totalBicis.textContent = total;
-}*/
-
 function agregarAlCarrito(bicis, precio) {
     var carrito = document.getElementById('carrito-lista');
     var totalBicis = document.getElementById('total');
